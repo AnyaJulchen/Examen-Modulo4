ef0fb023967658a1bca18ba27cc3205f3e72d98b
AnyaJulchen <155109518+AnyaJulchen@users.noreply.github.com>
2025-09-13 20:17:17 -0300

    Version actualizada con navegación

--- app/src/main/java/com/otec/examen_modulo4/MainActivity2.java
+++ app/src/main/java/com/otec/examen_modulo4/MainActivity2.java
@@ -19,19 +19,14 @@ public class MainActivity2 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_creditos)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
-
 }
--- app/src/main/java/com/otec/examen_modulo4/Menu.java
+++ app/src/main/java/com/otec/examen_modulo4/Menu.java
@@ -11,7 +11,7 @@ import androidx.navigation.fragment.NavHostFragment;
 // import androidx.navigation.ui.AppBarConfiguration; // Removed
 import androidx.navigation.ui.NavigationUI;
 
-import com.otec.examen_modulo4.databinding.ActivityMenuBinding;
+import com.otec.examen_modulo4.databinding.ActivityMainBinding;
 
 public class Menu extends AppCompatActivity {
 
@@ -21,15 +21,15 @@ public class Menu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        ActivityMenuBinding binding = ActivityMenuBinding.inflate(getLayoutInflater());
+        ActivityMainBinding binding = com.otec.examen_modulo4.databinding.ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.nav_host_fragment_activity_menu);
+                .findFragmentById(R.id.nav_host_fragment_activity_main);
         if (navHostFragment == null) {
             Log.e(TAG, "NavHostFragment not found with ID R.id.nav_host_fragment_activity_menu. Check your activity_menu.xml.");
-            return; // Exit onCreate if NavHostFragment is not found
+            return;
         }
         
         NavController navController = navHostFragment.getNavController();
